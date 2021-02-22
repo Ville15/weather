b11c20184533fd5c5c369d8ee5b600d34c0a67d0
Ville_L <n0luvi00@students.oamk.fi>
2021-02-22 16:15:50 +0200

    VALMIS TUOTOS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+const API_URL = 'https://api.openweathermap.org/data/2.5/weather?'
+const ICON_URL = 'https://openweather.org/img/wn/'
+const API_KEY = '9f0478009a5dc3d708793710bbb73969'
+
+function Weather () {
+  const [temp, setTemp] = useState(0)
+  const [speed, setSpeed] = useState(0)
+  const [direction, setDirection] = useState(0)
+  const [description, setDescription] = useState('')
+  const [icon, setIcon] = useState('')
+  const [isLoading, setIsLoading] = useState(true)
+  const [lat, setLat] = useState(0)
+  const [lng, setLng] = useState(0)
+
+  useEffect(() => {
+    const url =
+      API_URL +
+      'lat=' +
+      lat +
+      '&lon=' +
+      lng +
+      '&units=metric' +
+      '&appid=' +
+      API_KEY
+
+    fetch(url)
+      .then(res => res.json())
+      .then(
+        result => {
+          if (result.main != undefined) {
+            setTemp(result.main.temp)
+            setSpeed(result.wind.speed)
+            setDirection(result.wind.deg)
+            setDescription(result.weather[0].description)
+            setIcon(ICON_URL + result.weather[0].icon + '@2x.png')
+            setIsLoading(false)
+          } else {
+            alert('Could not read weather information')
+          }
+        },
+        error => {
+          setIsLoading(true)
+        }
+      )
+  }, [])
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          setLat(position.coords.latitude)
+          setLng(position.coords.longitude)
+        },
+        error => {}
+      )
+    } else {
+    }
+  }, [])
+
+  if (isLoading) {
+    return <p>Loading...</p>
+  } else {
+    return (
+      <div className='content'>
+        <h3>Your position is</h3>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Position: &nbsp;
+          {lat.toFixed(3)},{lng.toFixed(3)}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+        <>
+          <h3>Weather on your location</h3>
+          <p>{temp} C&#176;</p>
+          <p>
+            {speed} m/s {direction} degrees
+          </p>
+          <p>{description}</p>
+          <img src={icon} alt='' />
+        </>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default Weather
